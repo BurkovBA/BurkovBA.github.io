b5d1fb5780170fcb2c4a91c76e2ad3eec50018a9
Boris A. Burkov <vasjaforutube@gmail.com>
2017-02-19 23:37:08 +0000

    Basic DevOps.

---
+++ app/app.js
@@ -0,0 +1,62 @@
+(function(){
+angular.module("Universe", [
+    "ngAnimate",
+    "ngMessages",
+    "ngResource",
+    "ngSanitize",
+    "ngCookies",
+    "validation.match",
+    "ui.router",
+    "ui.bootstrap",
+    "gridshore.c3js.chart",
+    "ui.footable",
+    "schemaForm",
+    "ngFileUpload",
+    "ngScrollbars"
+])
+.config(function($urlRouterProvider, $locationProvider, $stateProvider, $httpProvider, $resourceProvider) {
+    // In production we may set a default route:
+    $urlRouterProvider.otherwise("/about");
+
+    // In production router may use html5 history API instead of hash routing:
+    // $locationProvider.html5Mode(true);
+
+    // Make $http ajax requests send django csrf token
+    $httpProvider.defaults.xsrfCookieName = "csrftoken";
+    $httpProvider.defaults.xsrfHeaderName = "X-CSRFToken";
+
+    // Django backend expects trailing slashes in URLs,
+    // e.g. '/api/user/', not '/api/user'. If django sees request to
+    // '/api/user', it would redirect request to '/api/user/', unless that's
+    // a POST request - POST will just fail.
+    // For instructions on re-configuration of django backend see:
+    // http://masnun.com/2013/09/18/django-rest-framework-angularjs-resource-trailing-slash-problem.html
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+
+    // Just a test state
+    $stateProvider
+        .state("test", {
+            url: "/test",
+            template: "This is a test"
+        });
+})
+.run(function($rootScope, Auth) {
+    // Auth service is globally available and requests current user on every
+    // state change.
+    $rootScope.Auth = Auth;
+
+    // Set special class on landing page <body> - it has some modifications
+    // in css and those require '.landing-page' - see index.html and sass
+    // in assets/styles/_landing.scss.
+    $rootScope.$on("$stateChangeSuccess", function(event, currentState){
+        switch (currentState.name) {
+            case "about":
+                $rootScope.specialClass = "landing-page";
+                break;
+            default:
+                $rootScope.specialClass = "";
+                break;
+        }
+    });
+});
+})();
