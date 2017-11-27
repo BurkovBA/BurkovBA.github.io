c421ee70bf7f87a00593c6c881713cf4869f268d
Boris A. Burkov <vasjaforutube@gmail.com>
2017-11-27 22:16:40 +0000

    Fixed indents, created MinimalizeMenu component.

--- src/components/Header/components/DesktopMenu.jsx
+++ src/components/Header/components/DesktopMenu.jsx
@@ -4,96 +4,95 @@ import { Dropdown } from 'react-bootstrap';
 class DesktopMenu extends React.Component {
   render() {
     return (
-    <div className="navbar-right">
-      <ul className="nav navbar-nav no-borders">
-        <li uib-dropdown ng-if="Auth.isLoggedIn()">
-          <a className="label-menu-corner" href="#" uib-dropdown-toggle>
-            <i className="pe-7s-drawer"></i>
-            {/*{notifications.length && (<span className="label label-success">{notifications.length}</span>)}*/}
-          </a>
-          {/*<ul uib-dropdown-menu notifications className="hdropdown animated flipInX" ng-scrollbars ng-scrollbars-config="scrollbarsConfig"></ul>*/}
-        </li>
-        <li uib-dropdown ng-if="Auth.isLoggedIn()">
-          <a href="#" uib-dropdown-toggle>
-            <i className="pe-7s-keypad"></i>
-          </a>
-          <div uib-dropdown-menu className="hdropdown bigmenu animated flipInX">
-            <table>
-              <tbody>
+      <div className="navbar-right">
+        <ul className="nav navbar-nav no-borders">
+          <li uib-dropdown ng-if="Auth.isLoggedIn()">
+            <a className="label-menu-corner" href="#" uib-dropdown-toggle>
+              <i className="pe-7s-drawer"></i>
+              {/*{notifications.length && (<span className="label label-success">{notifications.length}</span>)}*/}
+            </a>
+            {/*<ul uib-dropdown-menu notifications className="hdropdown animated flipInX" ng-scrollbars ng-scrollbars-config="scrollbarsConfig"></ul>*/}
+          </li>
+          <li uib-dropdown ng-if="Auth.isLoggedIn()">
+            <a href="#" uib-dropdown-toggle>
+              <i className="pe-7s-keypad"></i>
+            </a>
+            <div uib-dropdown-menu className="hdropdown bigmenu animated flipInX">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <a ui-sref="app_views.projects">
+                        <i className="pe pe-7s-portfolio text-info"></i>
+                        <h5>Projects</h5>
+                      </a>
+                    </td>
+                    <td>
+                      <a ui-sref="app_views.mailbox">
+                        <i className="pe pe-7s-mail text-warning"></i>
+                        <h5>Email</h5>
+                      </a>
+                    </td>
+                    <td>
+                      <a ui-sref="app_views.contacts">
+                        <i className="pe pe-7s-users text-success"></i>
+                        <h5>Contacts</h5>
+                      </a>
+                    </td>
+                  </tr>
                 <tr>
                   <td>
-                    <a ui-sref="app_views.projects">
-                      <i className="pe pe-7s-portfolio text-info"></i>
-                      <h5>Projects</h5>
+                    <a ui-sref="app_views.forum">
+                      <i className="pe pe-7s-comment text-info"></i>
+                      <h5>Forum</h5>
                     </a>
                   </td>
                   <td>
-                    <a ui-sref="app_views.mailbox">
-                      <i className="pe pe-7s-mail text-warning"></i>
-                      <h5>Email</h5>
+                    <a ui-sref="analytics">
+                      <i className="pe pe-7s-graph1 text-danger"></i>
+                      <h5>Analytics</h5>
                     </a>
                   </td>
                   <td>
-                    <a ui-sref="app_views.contacts">
-                      <i className="pe pe-7s-users text-success"></i>
-                      <h5>Contacts</h5>
+                    <a ui-sref="app_views.file_manager">
+                      <i className="pe pe-7s-box1 text-success"></i>
+                      <h5>Files</h5>
                     </a>
                   </td>
                 </tr>
-              <tr>
-                <td>
-                  <a ui-sref="app_views.forum">
-                    <i className="pe pe-7s-comment text-info"></i>
-                    <h5>Forum</h5>
-                  </a>
-                </td>
-                <td>
-                  <a ui-sref="analytics">
-                    <i className="pe pe-7s-graph1 text-danger"></i>
-                    <h5>Analytics</h5>
-                  </a>
-                </td>
-                <td>
-                  <a ui-sref="app_views.file_manager">
-                    <i className="pe pe-7s-box1 text-success"></i>
-                    <h5>Files</h5>
-                  </a>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-        </li>
-
-        <li uib-dropdown ng-if="Auth.isLoggedIn()">
-          <a href="#" uib-dropdown-toggle>
-            <i className="pe-7s-user"></i>
-          </a>
-          <ul uib-dropdown-menu className="hdropdown animated flipInX">
-            <div className="title">
-              Account menu
+                </tbody>
+              </table>
             </div>
-            <li>
-              <a href="https://mybg.devbg.us/user/profile">
-                <i className="pe pe-7s-edit"></i> &nbsp;&nbsp; Account settings
-              </a>
-            </li>
-            <li>
-              <a ng-click="Auth.logout()">
-                <i className="pe pe-7s-next-2"></i> &nbsp;&nbsp;&nbsp; Sign out
-              </a>
-            </li>
-          </ul>
-        </li>
+          </li>
 
-        <li ng-if="!Auth.isLoggedIn()">
-          <a ng-click="login()">
-            <i className="pe-7s-upload pe-rotate-90"></i>
-          </a>
-        </li>
-      </ul>
-    </div>
+          <li uib-dropdown ng-if="Auth.isLoggedIn()">
+            <a href="#" uib-dropdown-toggle>
+              <i className="pe-7s-user"></i>
+            </a>
+            <ul uib-dropdown-menu className="hdropdown animated flipInX">
+              <div className="title">
+                Account menu
+              </div>
+              <li>
+                <a href="https://mybg.devbg.us/user/profile">
+                  <i className="pe pe-7s-edit"></i> &nbsp;&nbsp; Account settings
+                </a>
+              </li>
+              <li>
+                <a ng-click="Auth.logout()">
+                  <i className="pe pe-7s-next-2"></i> &nbsp;&nbsp;&nbsp; Sign out
+                </a>
+              </li>
+            </ul>
+          </li>
 
+          <li ng-if="!Auth.isLoggedIn()">
+            <a ng-click="login()">
+              <i className="pe-7s-upload pe-rotate-90"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,52 +2,53 @@ import React from 'react';
 import { Dropdown } from 'react-bootstrap';
 import MobileMenu from "components/Header/components/MobileMenu.jsx";
 import DesktopMenu from "components/Header/components/DesktopMenu.jsx";
+import MinimalizeMenu from "components/Header/components/MinimalizeMenu.jsx";
 
 class Header extends React.Component {
 
-    toggleNavigation(e) {
-        e.preventDefault();
-        $("body").toggleClass("mini-navbar");
-    }
+  toggleNavigation(e) {
+    e.preventDefault();
+    $("body").toggleClass("mini-navbar");
+  }
 
-    render() {
-        return [
-            <div className="color-line"></div>,
-            <div id="logo" className="light-version">
-                    <span>
-                        Burkov<font color="red">.net</font>
-                    </span>
-            </div>,
-            <nav role="navigation">
-                <MinimalizaMenu/>
-                <div className="small-logo">
-                    <span className="text-primary">Burkov<font color="red">.net</font><br/></span>
-                </div>
-                <form role="search" className="navbar-form-custom" method="post" action="#">
-                    <div className="form-group">
-                        <input type="text" placeholder="Find ..." className="form-control" name="search"/>
-                    </div>
-                </form>
-                <MobileMenu/>
-                <DesktopMenu/>
-            </nav>,
+  render() {
+    return [
+      <div className="color-line"></div>,
+      <div id="logo" className="light-version">
+        <span>
+          Burkov<font color="red">.net</font>
+        </span>
+      </div>,
+      <nav role="navigation">
+        <MinimalizeMenu/>
+        <div className="small-logo">
+          <span className="text-primary">Burkov<font color="red">.net</font><br/></span>
+        </div>
+        <form role="search" className="navbar-form-custom" method="post" action="#">
+          <div className="form-group">
+            <input type="text" placeholder="Find ..." className="form-control" name="search"/>
+          </div>
+        </form>
+        <MobileMenu/>
+        <DesktopMenu/>
+      </nav>,
 
-            <div className="row border-bottom">
-                <nav className="navbar navbar-static-top white-bg" role="navigation" style={{marginBottom: 0}}>
-                    <div className="navbar-header">
-                        <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " onClick={this.toggleNavigation} href="#"><i className="fa fa-bars"></i> </a>
-                    </div>
-                    <ul className="nav navbar-top-links navbar-right">
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-sign-out"></i> Log out
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        ];
-    }
+      <div className="row border-bottom">
+        <nav className="navbar navbar-static-top white-bg" role="navigation" style={{marginBottom: 0}}>
+          <div className="navbar-header">
+            <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " onClick={this.toggleNavigation} href="#"><i className="fa fa-bars"></i> </a>
+          </div>
+          <ul className="nav navbar-top-links navbar-right">
+            <li>
+              <a href="#">
+                <i className="fa fa-sign-out"></i> Log out
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    ];
+  }
 }
 
 export default Header
