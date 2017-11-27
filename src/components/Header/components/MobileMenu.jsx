aa07f022c64d13a6361b821c1f4afef45b265b9a
Boris A. Burkov <vasjaforutube@gmail.com>
2017-11-27 11:17:05 +0000

    Splitting Header into subcomponents.

---
+++ src/components/Header/components/MobileMenu.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+
+class MobileMenu extends React.Component {
+  render() {
+    return (
+      <div className="mobile-menu">
+        <button type="button" className="navbar-toggle mobile-menu-toggle" ng-click="collapseMobileMenu = !collapseMobileMenu">
+          <i className="fa fa-chevron-down"></i>
+        </button>
+        <div uib-collapse="collapseMobileMenu" className="collapse mobile-navbar">
+          <ul className="nav navbar-nav">
+            <li ng-if="!Auth.isLoggedIn()">
+              <a ng-click="login()">Login</a>
+            </li>
+            <li ng-if="Auth.isLoggedIn()">
+              <a ng-click="Auth.logout()">Logout</a>
+            </li>
+            <li ng-if="Auth.isLoggedIn()">
+              <a href="https://mybg.devbg.us/user/profile">Settings</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MobileMenu;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
+import MobileMenu from "components/Header/components/MobileMenu.jsx";
+import DesktopMenu from "components/Header/components/DesktopMenu.jsx";
 
 class Header extends React.Component {
 
@@ -10,132 +12,24 @@ class Header extends React.Component {
 
     render() {
         return [
-            <div class="color-line"></div>,
-            <div id="logo" class="light-version">
+            <div className="color-line"></div>,
+            <div id="logo" className="light-version">
                     <span>
                         Burkov<font color="red">.net</font>
                     </span>
             </div>,
             <nav role="navigation">
-                    <minimaliza-menu></minimaliza-menu>
-                <div class="small-logo">
-                    <span class="text-primary">Burkov<font color="red">.net</font><br/></span>
+                <MinimalizaMenu/>
+                <div className="small-logo">
+                    <span className="text-primary">Burkov<font color="red">.net</font><br/></span>
                 </div>
-                <form role="search" class="navbar-form-custom" method="post" action="#">
-                    <div class="form-group">
-                        <input type="text" placeholder="Find ..." class="form-control" name="search"/>
+                <form role="search" className="navbar-form-custom" method="post" action="#">
+                    <div className="form-group">
+                        <input type="text" placeholder="Find ..." className="form-control" name="search"/>
                     </div>
                 </form>
-                <div class="mobile-menu">
-                    <button type="button" class="navbar-toggle mobile-menu-toggle" ng-click="collapseMobileMenu = !collapseMobileMenu">
-                        <i class="fa fa-chevron-down"></i>
-                    </button>
-                    <div uib-collapse="collapseMobileMenu" class="collapse mobile-navbar">
-                        <ul class="nav navbar-nav">
-                            <li ng-if="!Auth.isLoggedIn()">
-                                <a ng-click="login()">Login</a>
-                            </li>
-                            <li ng-if="Auth.isLoggedIn()">
-                                <a ng-click="Auth.logout()">Logout</a>
-                            </li>
-                            <li ng-if="Auth.isLoggedIn()">
-                                <a href="https://mybg.devbg.us/user/profile">Settings</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="navbar-right">
-                    <ul class="nav navbar-nav no-borders">
-                        <li uib-dropdown ng-if="Auth.isLoggedIn()">
-                            <a class="label-menu-corner" href="#" uib-dropdown-toggle>
-                                <i class="pe-7s-drawer"></i>
-                                {notifications.length && (<span class="label label-success">{notifications.length}</span>)}
-                            </a>
-                            <ul uib-dropdown-menu notifications class="hdropdown animated flipInX" ng-scrollbars ng-scrollbars-config="scrollbarsConfig"></ul>
-                        </li>
-                        <li uib-dropdown ng-if="Auth.isLoggedIn()">
-                            <a href="#" uib-dropdown-toggle>
-                                <i class="pe-7s-keypad"></i>
-                            </a>
-                            <div uib-dropdown-menu class="hdropdown bigmenu animated flipInX">
-
-                                <table>
-                                    <tbody>
-                                    <tr>
-                                        <td>
-                                            <a ui-sref="app_views.projects">
-                                                <i class="pe pe-7s-portfolio text-info"></i>
-                                                <h5>Projects</h5>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a ui-sref="app_views.mailbox">
-                                                <i class="pe pe-7s-mail text-warning"></i>
-                                                <h5>Email</h5>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a ui-sref="app_views.contacts">
-                                                <i class="pe pe-7s-users text-success"></i>
-                                                <h5>Contacts</h5>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <a ui-sref="app_views.forum">
-                                                <i class="pe pe-7s-comment text-info"></i>
-                                                <h5>Forum</h5>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a ui-sref="analytics">
-                                                <i class="pe pe-7s-graph1 text-danger"></i>
-                                                <h5>Analytics</h5>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a ui-sref="app_views.file_manager">
-                                                <i class="pe pe-7s-box1 text-success"></i>
-                                                <h5>Files</h5>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </li>
-
-                        <li uib-dropdown ng-if="Auth.isLoggedIn()">
-                            <a href="#" uib-dropdown-toggle>
-                                <i class="pe-7s-user"></i>
-                            </a>
-                            <ul uib-dropdown-menu class="hdropdown animated flipInX">
-                                <div class="title">
-                                    Account menu
-                                </div>
-                                <li>
-                                    <a href="https://mybg.devbg.us/user/profile">
-                                        <i class="pe pe-7s-edit"></i> &nbsp;&nbsp; Account settings
-                                    </a>
-                                </li>
-                                <li>
-                                    <a ng-click="Auth.logout()">
-                                        <i class="pe pe-7s-next-2"></i> &nbsp;&nbsp;&nbsp; Sign out
-                                    </a>
-                                </li>
-                            </ul>
-
-                        </li>
-
-                        <li ng-if="!Auth.isLoggedIn()">
-                            <a ng-click="login()">
-                                <i class="pe-7s-upload pe-rotate-90"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                <MobileMenu/>
+                <DesktopMenu/>
             </nav>,
 
             <div className="row border-bottom">
