50c3fa1fa9e90b71f202c6a7b2fa90c544980f80
Boris A. Burkov <vasjaforutube@gmail.com>
2017-11-28 10:53:12 +0000

    Fixed indents in navigation, trying to fix its layout.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,12 +5,6 @@ import DesktopMenu from "components/Header/components/DesktopMenu.jsx";
 import MinimalizeMenu from "components/Header/components/MinimalizeMenu.jsx";
 
 class Header extends React.Component {
-
-  toggleNavigation(e) {
-    e.preventDefault();
-    $("body").toggleClass("mini-navbar");
-  }
-
   render() {
     return [
       <div className="color-line"></div>,
@@ -31,22 +25,7 @@ class Header extends React.Component {
         </form>
         <MobileMenu/>
         <DesktopMenu/>
-      </nav>,
-
-      <div className="row border-bottom">
-        <nav className="navbar navbar-static-top white-bg" role="navigation" style={{marginBottom: 0}}>
-          <div className="navbar-header">
-            <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " onClick={this.toggleNavigation} href="#"><i className="fa fa-bars"></i> </a>
-          </div>
-          <ul className="nav navbar-top-links navbar-right">
-            <li>
-              <a href="#">
-                <i className="fa fa-sign-out"></i> Log out
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      </nav>
     ];
   }
 }
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -4,70 +4,71 @@ import { Link, Location } from 'react-router-dom';
 
 class Navigation extends Component {
 
-    componentDidMount() {
-        const { menu } = this.refs;
-        $(menu).metisMenu();
+  componentDidMount() {
+    const { menu } = this.refs;
+    $(menu).metisMenu();
 
-        // Collapse menu in mobile mode after click on element
-        let menuElement = $('#side-menu a:not([href$="\\#"])');
-        menuElement.click(function(){
-            if ($(window).width() < 769) {
-                $("body").toggleClass("show-sidebar");
-            }
-        });
-    }
+    // Collapse menu in mobile mode after click on element
+    let menuElement = $('#side-menu a:not([href$="\\#"])');
+    menuElement.click(function(){
+      if ($(window).width() < 769) {
+        $("body").toggleClass("show-sidebar");
+      }
+    });
+  }
 
-    activeRoute(routeName) {
-        return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
-    }
+  activeRoute(routeName) {
+    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  }
 
-    secondLevelActive(routeName) {
-        return this.props.location.pathname.indexOf(routeName) > -1 ? "nav nav-second-level collapse in" : "nav nav-second-level collapse";
-    }
+  secondLevelActive(routeName) {
+    return this.props.location.pathname.indexOf(routeName) > -1 ? "nav nav-second-level collapse in" : "nav nav-second-level collapse";
+  }
 
-    render() {
-        return [
-            <div className="profile-picture">
-                <a href="index.html">
-                    <img src="/src/images/burkov_boris_web.jpg" className="img-circle m-b" alt="logo"></img>
-                </a>
+  render() {
+    return (
+      <div id="navigation">
+        <div className="profile-picture">
+          <a href="index.html">
+            <img src="/src/images/burkov_boris_web.jpg" className="img-circle m-b" alt="logo"></img>
+          </a>
 
-                <div className="stats-label text-color">
-                    <span className="font-extra-bold font-uppercase">Борис Бурков</span>
+          <div className="stats-label text-color">
+            <span className="font-extra-bold font-uppercase">Борис Бурков</span>
+            <div className="dropdown">
+              <a href="/about">
+                <small className="text-muted">Обо мне</small>
+              </a>
+            </div>
+          </div>
+        </div>
 
-                    <div className="dropdown">
-                        <a href="/about">
-                            <small className="text-muted">Обо мне</small>
-                        </a>
-                    </div>
-                </div>
-            </div>,
-
-            <ul className="nav" id="side-menu">
-                <li className={this.activeRoute("/main")}>
-                    <Link to="/world"><i className="fa fa-th-large"></i> <span className="nav-label">Как работает мир</span></Link>
-                </li>
-                <li className={this.activeRoute("/software-engineering")}>
-                    <Link to="/software-engineering"><i className="fa fa-th-large"></i> <span className="nav-label">Программирование</span></Link>
-                </li>
-                <li className={this.activeRoute("/economy")}>
-                    <Link to="/economy"><i className="fa fa-th-large"></i> <span className="nav-label">Экономика и бизнес</span></Link>
-                </li>
-                <li className={this.activeRoute("/biomed")}>
-                    <Link to="/biomed"><i className="fa fa-th-large"></i> <span className="nav-label">Биология и медицина</span></Link>
-                </li>
-                <li className={this.activeRoute("/music")}>
-                    <Link to="/music"><i className="fa fa-th-large"></i> <span className="nav-label">Музыка</span></Link>
-                </li>
-                <li className={this.activeRoute("/history")}>
-                    <Link to="/history"><i className="fa fa-th-large"></i> <span className="nav-label">История</span></Link>
-                </li>
-                <li className={this.activeRoute("/diary")}>
-                    <Link to="/diary"><i className="fa fa-th-large"></i> <span className="nav-label">Дневник</span></Link>
-                </li>
-            </ul>
-        ];
-    }
+        <ul className="nav" id="side-menu">
+          <li className={this.activeRoute("/main")}>
+            <Link to="/world"><i className="fa fa-th-large"></i> <span className="nav-label">Как работает мир</span></Link>
+          </li>
+          <li className={this.activeRoute("/software-engineering")}>
+            <Link to="/software-engineering"><i className="fa fa-th-large"></i> <span className="nav-label">Программирование</span></Link>
+          </li>
+          <li className={this.activeRoute("/economy")}>
+            <Link to="/economy"><i className="fa fa-th-large"></i> <span className="nav-label">Экономика и бизнес</span></Link>
+          </li>
+          <li className={this.activeRoute("/biomed")}>
+            <Link to="/biomed"><i className="fa fa-th-large"></i> <span className="nav-label">Биология и медицина</span></Link>
+          </li>
+          <li className={this.activeRoute("/music")}>
+            <Link to="/music"><i className="fa fa-th-large"></i> <span className="nav-label">Музыка</span></Link>
+          </li>
+          <li className={this.activeRoute("/history")}>
+            <Link to="/history"><i className="fa fa-th-large"></i> <span className="nav-label">История</span></Link>
+          </li>
+          <li className={this.activeRoute("/diary")}>
+            <Link to="/diary"><i className="fa fa-th-large"></i> <span className="nav-label">Дневник</span></Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Navigation
+export default Navigation;
