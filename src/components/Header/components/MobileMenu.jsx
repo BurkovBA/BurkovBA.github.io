5fcf6e2c4073436af721d552f6caa0cf4769d5d5
Boris A. Burkov <vasjaforutube@gmail.com>
2017-12-05 17:43:24 +0000

    Commented-out contents of MobileMenu, as it needn't exist yet.

--- src/components/Header/components/MobileMenu.jsx
+++ src/components/Header/components/MobileMenu.jsx
@@ -5,22 +5,22 @@ class MobileMenu extends React.Component {
   render() {
     return (
       <div className="mobile-menu">
-        <button type="button" className="navbar-toggle mobile-menu-toggle">
-          <i className="fa fa-chevron-down"></i>
-        </button>
-        <div uib-collapse="collapseMobileMenu" className="collapse mobile-navbar">
-          <ul className="nav navbar-nav">
-            <li ng-if="!Auth.isLoggedIn()">
-              <a ng-click="login()">Login</a>
-            </li>
-            <li ng-if="Auth.isLoggedIn()">
-              <a ng-click="Auth.logout()">Logout</a>
-            </li>
-            <li ng-if="Auth.isLoggedIn()">
-              <a href="https://mybg.devbg.us/user/profile">Settings</a>
-            </li>
-          </ul>
-        </div>
+        {/*<button type="button" className="navbar-toggle mobile-menu-toggle">*/}
+          {/*<i className="fa fa-chevron-down"></i>*/}
+        {/*</button>*/}
+        {/*<div uib-collapse="collapseMobileMenu" className="collapse mobile-navbar">*/}
+          {/*<ul className="nav navbar-nav">*/}
+            {/*<li ng-if="!Auth.isLoggedIn()">*/}
+              {/*<a ng-click="login()">Login</a>*/}
+            {/*</li>*/}
+            {/*<li ng-if="Auth.isLoggedIn()">*/}
+              {/*<a ng-click="Auth.logout()">Logout</a>*/}
+            {/*</li>*/}
+            {/*<li ng-if="Auth.isLoggedIn()">*/}
+              {/*<a href="https://mybg.devbg.us/user/profile">Settings</a>*/}
+            {/*</li>*/}
+          {/*</ul>*/}
+        {/*</div>*/}
       </div>
     )
   }
