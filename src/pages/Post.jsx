ca191728326d7c22908b1a8b7d6f50ac93f3efd0
Boris A. Burkov <vasjaforutube@gmail.com>
2017-12-06 22:54:52 +0000

    Added Disqus comments to Post.

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -41,6 +41,26 @@ class Post extends React.Component {
       id: this.props.match.params.id,
       post: posts[this.props.match.params.id].default
     });
+
+    self.initializeDisqus();
+  }
+
+  initializeDisqus() {
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+
+    // DON'T EDIT BELOW THIS LINE
+    let d = document, s = d.createElement('script');
+    s.src = 'https://burkovba.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
   }
 
   /**
@@ -86,6 +106,7 @@ class Post extends React.Component {
               </div>
           </div>
         </div>
+        <div id="disqus_thread"></div>
       </div>
     )
   }
