7dcb2e0c9a47fd42ed84bba987967321d31ed826
Boris A. Burkov <vasjaforutube@gmail.com>
2017-12-09 15:16:21 +0000

    #1 Animated Yes/No buttons on modal.

--- src/components/HidePostsInRussianModal/index.jsx
+++ src/components/HidePostsInRussianModal/index.jsx
@@ -4,10 +4,12 @@ import {Modal, Button} from 'react-bootstrap';
 class HidePostsInRussianModal extends Component {
   yes() {
       localStorage.setItem('hidePostsInRussian', true);
+      this.props.onHide();
   }
 
   no() {
-      localStorage.removeItem('hidePostsInRussian')
+      localStorage.removeItem('hidePostsInRussian');
+      this.props.onHide();
   }
 
   render () {
@@ -38,8 +40,8 @@ class HidePostsInRussianModal extends Component {
           <p>Do you want to hide blog posts <strong>in russian</strong>?</p>
         </Modal.Body>
         <Modal.Footer className="modal-footer">
-          <Button bsStyle="default" onClick={this.yes()}>Yes</Button>
-          <Button bsStyle="primary"onClick={this.no()}>No</Button>
+          <Button bsStyle="default" onClick={() => this.yes()}>Yes</Button>
+          <Button bsStyle="primary"onClick={() => this.no()}>No</Button>
         </Modal.Footer>
       </Modal>
     )
