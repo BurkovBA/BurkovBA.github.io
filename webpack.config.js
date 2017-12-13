ba6438dc7bd8f0bd0d58e14036d3542997aafe56
Boris A. Burkov <vasjaforutube@gmail.com>
2017-12-13 00:41:35 +0000

    Created npm deploy command to ship updates to github in one go.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'app.jsx'),
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: 'dist/',
+    publicPath: '/dist/',
     filename: 'app.[hash:7].js'
   },
   resolve: {
