2c0635dc89ce299d273bd06282dc31764a10158d
Boris A. Burkov <vasjaforutube@gmail.com>
2017-12-14 23:53:42 +0000

    Partially fixed splitting blog posts into columns.

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -9,9 +9,9 @@ class Blog extends React.Component {
   render() {
     return (
       <div className="row">
-        { Object.values(this.props.posts).map((post) =>
-          <div key={post.metadata.id} className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <div className="hpanel blog-box">
+        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          { Object.values(this.props.posts).filter((el, i) => { return i % 2 === 0; }).map((post) =>
+            <div key={post.metadata.id} className="hpanel blog-box">
               <div className="panel-heading">
                 <div className="media clearfix">
                   <a className="pull-left">
@@ -54,8 +54,55 @@ class Blog extends React.Component {
                 <Link to={`/blog/${post.metadata.id}`}> <h4>{tr('Continue')}...</h4></Link>
               </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          { Object.values(this.props.posts).filter((el, i) => { return i % 2 === 1; }).map((post) =>
+            <div key={post.metadata.id} className="hpanel blog-box">
+              <div className="panel-heading">
+                <div className="media clearfix">
+                  <a className="pull-left">
+                    <img src={ post.metadata.authors_avatar } alt="profile-picture" />
+                  </a>
+                  <div className="media-body">
+                    <span className="post-authorship">
+                      <small><span className="font-bold">{ tr(post.metadata.author) }</span> </small>
+                      <br />
+                      <small className="text-muted"><i className="fa fa-calendar"> </i> { post.metadata.date_created }</small>
+                    </span>
+                    <span className="pull-right">
+                      <small><i className="fa fa-clock-o"> </i> {tr('Time to read')}: { post.metadata.time_to_read } min</small>
+                      <br />
+                      <small>{ post.metadata.categories.map((category, index) =>
+                        <span key={category}>
+                          <span>{ !!index && ' ' }</span>
+                          <span className={`label label-${category}`}>{ tr(category) }</span>
+                        </span>
+                      )}
+                      </small>
+                      {/*<small><i className="fa fa-eye"> </i> Просмотров: 62 </small>*/}
+                      {/*<br />*/}
+                      {/*<small><i className="fa fa-comments-o"> </i> Комментариев: 10</small>*/}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="panel-image">
+                <img className="img-responsive" src={ post.metadata.cover } alt="" />
+                <div className="title">
+                  <Link to={`/blog/${post.metadata.id}`}> <h4>{ post.metadata.title }</h4></Link>
+                  <small>{ post.metadata.subtitle }</small>
+                </div>
+              </div>
+              <div className="panel-body">
+                <p>
+                  { post.metadata.abstract }
+                </p>
+                <Link to={`/blog/${post.metadata.id}`}> <h4>{tr('Continue')}...</h4></Link>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
