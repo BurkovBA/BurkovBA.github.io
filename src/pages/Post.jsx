292e2b10ddd062153616f8fc1f390eef832cb7f3
Boris A. Burkov <vasjaforutube@gmail.com>
2017-12-17 23:57:56 +0000

    Make abstract text italic.

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -101,7 +101,7 @@ class Post extends React.Component {
               </div>
             </div>
               <div className="panel-body">
-                <p>
+                <p class="abstract">
                   { this.state.abstract }
                 </p>
                 <hr />
