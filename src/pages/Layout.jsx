943544d2b3e2f851469f5d64467715a5897ca40b
Boris A. Burkov <vasjaforutube@gmail.com>
2017-12-18 19:40:09 +0000

    #5 Added Hotjar tracking code.

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -61,6 +61,7 @@ class Layout extends React.Component {
 
   componentWillMount() {
     this.initializeGoogleAnalytics();
+    this.initializeHotjar();
   }
 
   initializeGoogleAnalytics() {
@@ -75,6 +76,18 @@ class Layout extends React.Component {
     gtag('js', new Date());
 
     gtag('config', 'UA-111349824-1');
+  }
+
+  initializeHotjar() {
+    // Hotjar Tracking Code for https://burkovba.github.io
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:726575,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
   }
 
   componentDidMount() {
