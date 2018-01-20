b55bb88542dcb22540333d5c87daec5f223477dc
Boris A. Burkov <vasjaforutube@gmail.com>
2018-01-20 22:29:31 +0000

    Finished OpenStack key concepts.

--- src/pages/posts/2018-01-19-1/index.jsx
+++ src/pages/posts/2018-01-19-1/index.jsx
@@ -43,22 +43,34 @@ class Content extends React.Component {
         <p>
           OpenStack evolves over time and has seen many <a href="https://releases.openstack.org/">releases</a>.
           OpenStack consists of a <a href="https://www.openstack.org/software/project-navigator/">list of APIs, called Projects</a> for
-          managing different aspects of you cloud infrastructure. Arguably the most important among them are
-          <a href="https://www.openstack.org/software/releases/ocata/components/nova">Nova</a> used to create new
-          virtual machines and <a href="https://www.openstack.org/software/releases/ocata/components/cinder">Cinder</a> used
+          managing different aspects of you cloud infrastructure. Arguably the most important among them are <a href="https://www.openstack.org/software/releases/ocata/components/nova">Nova</a> used
+          to create new virtual machines and <a href="https://www.openstack.org/software/releases/ocata/components/cinder">Cinder</a> used
           to manage block devices.
         </p>
+        <h4>Key concepts</h4>
         <p>
           I'll briefly list
           the <a href="https://netapp.github.io/openstack-deploy-ops-guide/kilo/content/section_nova-key-concepts.html">key concepts of Nova</a> and <a href="https://netapp.github.io/openstack-deploy-ops-guide/kilo/content/section_cinder-key-concepts.html">key concepts of Cinder</a> here.
         </p>
         <ul>
           <li><b>instance</b> - a virtual server</li>
-          <li><b>flavor</b> - on Rackspace you can't just say I want a server with X MB RAM, Y processor, Z hard drive - you
-            have to choose one from the existing presets - flavors - e.g. 32 MB RAM, 4 core processor, 20 GB hard drive
+          <li><b>flavor</b> - in OpenStack you can't just say I want a server with X MB RAM, Y processor, Z hard drive - you
+            have to choose one from the existing presets - flavors - e.g. 32 MB RAM, 4 core processor, 20 GB hard drive (<a href="https://netapp.github.io/openstack-deploy-ops-guide/kilo/content/section_nova-key-concepts.html">see more here</a>).
           </li>
-          <li></li>
+          <li><b>disk</b> - every instance needs at least a root disk that contains boot loader and operating system to boot;
+            root disk can be represented either by an <b>ephemeral disk</b> that will be destroyed when the instance is
+              destroyed or by <b>persistent volume</b> that will survive the destruction of instance (<a href="https://netapp.github.io/openstack-deploy-ops-guide/kilo/content/section_cinder-key-concepts.html">see more here</a>).
+          </li>
+          <li><b>volume</b> - persistent Cinder volume; by contrast with ephemeral disks, contents of volumes are
+              persistent and survive instance destruction (<a href="https://netapp.github.io/openstack-deploy-ops-guide/kilo/content/section_nova-deployment-choices.html#nova.boot.options">see more here</a>).
+          </li>
+          <li><b>image</b> - image of an operating system, similar to .iso images that you install your OS from.</li>
+          <li><b>snapshot</b> - copy of a disk or volume, taken at a certain moment of time; could be used to create
+            a root volume from it.
+          </li>
         </ul>
+        <h4>Python APIs</h4>
+        <h4>Terraform</h4>
       </div>
     )
   }
