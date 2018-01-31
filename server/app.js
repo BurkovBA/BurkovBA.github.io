0db0f6443c79f8d9e26c101ec322aa928ea79e57
Boris A. Burkov <vasjaforutube@gmail.com>
2018-01-31 23:18:37 +0000

    Leftover import, link to a blog post.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
+import path from 'path';
+// import favicon from 'serve-favicon'
 
 // Mostly stolen from:
 // https://medium.com/@phoebe.greig/headache-free-isomorphic-app-tutorial-react-js-react-router-node-js-ssr-with-state-and-es6-797a8d8e493a
+// https://crypt.codemancers.com/posts/2017-06-03-reactjs-server-side-rendering-with-router-v4-and-redux/
 const app = express();
 const assets = express.static(path.join(__dirname, '../dist'));
+const port = process.env.PORT || 80;
 
 app.use(assets);
-
-const port = process.env.PORT || 80;
 
 app.listen(port);
 console.log(`Listening at http://localhost:${port}`);
