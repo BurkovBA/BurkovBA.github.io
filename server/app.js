f59b73a5f3854bc8e6882bdc674b49a5676db833
Boris A. Burkov <vasjaforutube@gmail.com>
2018-02-07 10:08:21 +0000

    Working on server-side rendering.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,32 @@
 import express from 'express';
 import path from 'path';
-import { renderToString } from 'react-dom/server';
-import StaticRouter from 'react-router-dom/StaticRouter';
 // import favicon from 'serve-favicon'
+
+import { renderToString } from 'react-dom/server';
+import {renderRoutes} from 'react-router-config';
+import StaticRouter from 'react-router-dom/StaticRouter';
+
 
 // Mostly stolen from:
 // https://medium.com/@phoebe.greig/headache-free-isomorphic-app-tutorial-react-js-react-router-node-js-ssr-with-state-and-es6-797a8d8e493a
 // https://crypt.codemancers.com/posts/2017-06-03-reactjs-server-side-rendering-with-router-v4-and-redux/
+
+function expressRouter(req, res) {
+
+}
+
+const routes = [
+  {
+    component: Layout,
+    routes: [
+      { path: "/blog", component: Blog, exact: true },
+      { path: "/blog/:id", component: Post},
+      { path: "/about", component: About }
+    ]
+  }
+]
+
+
 const app = express();
 const assets = express.static(path.join(__dirname, '../dist'));
 const port = process.env.PORT || 80;
