3fb4b1dcad0ce934197ff8168905728b319c834c
Boris A. Burkov <vasjaforutube@gmail.com>
2018-02-07 22:25:13 +0000

    Basic express SSR implementation.

--- server/app.jsx
+++ server/app.jsx
@@ -3,17 +3,13 @@ import path from 'path';
 // import favicon from 'serve-favicon'
 
 import { renderToString } from 'react-dom/server';
-import {renderRoutes} from 'react-router-config';
+import { renderRoutes } from 'react-router-config';
 import StaticRouter from 'react-router-dom/StaticRouter';
 
 
 // Mostly stolen from:
 // https://medium.com/@phoebe.greig/headache-free-isomorphic-app-tutorial-react-js-react-router-node-js-ssr-with-state-and-es6-797a8d8e493a
 // https://crypt.codemancers.com/posts/2017-06-03-reactjs-server-side-rendering-with-router-v4-and-redux/
-
-function expressRouter(req, res) {
-
-}
 
 const routes = [
   {
@@ -24,14 +20,26 @@ const routes = [
       { path: "/about", component: About }
     ]
   }
-]
+];
 
+const port = process.env.PORT || 80;
 
 const app = express();
 const assets = express.static(path.join(__dirname, '../dist'));
-const port = process.env.PORT || 80;
-
 app.use(assets);
 
+app.get('*', (req, res) => {
+  let context = {};
+
+  const content = renderToString(
+    <StaticRouter location={req.url} context={context}>
+      { renderRoutes(routes) }
+    </StaticRouter>
+  );
+
+  res.render('index', {title: 'Express', data: false, content })
+});
+
 app.listen(port);
+
 console.log(`Listening at http://localhost:${port}`);
