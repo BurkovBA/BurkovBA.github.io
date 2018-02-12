641efa75d229f2e931057d3c526fc18049f539a2
Boris A. Burkov <vasjaforutube@gmail.com>
2018-02-12 19:01:42 +0000

    Implemented RenderFullPath function, static assets middleware path is broken.

--- src/server/app.jsx
+++ src/server/app.jsx
@@ -28,11 +28,46 @@ const routes = [
   }
 ];
 
+
+function renderFullPage (html) {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Personal blog of Boris Burkov</title>
+    
+        <link rel="apple-touch-icon" sizes="57x57" href="/dist/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/dist/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/dist/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/dist/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/dist/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/dist/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/dist/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/dist/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/dist/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="/dist/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/dist/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/dist/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/dist/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/dist/favicons/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/dist/favicons/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        
+        <link rel="stylesheet" href="/dist/server.css">
+      </head>  
+      <body class="fixed-navbar fixed-sidebar">
+        <div id="main">${html}</div>
+      </body>
+    </html>`;
+}
+
 const port = process.env.PORT || 8080;
 
 const app = express();
-const assets = express.static(path.join(__dirname, '../dist'));
-app.use(assets);
+app.use('/dist', express.static(path.join(__dirname, '../')));
 
 app.get('*', (req, res) => {
   let context = {};
@@ -43,9 +78,10 @@ app.get('*', (req, res) => {
     </StaticRouter>
   );
 
-  res.status(200).send(content)
+  res.status(200).send(renderFullPage(content))
 });
 
 app.listen(port);
 
 console.log(`Listening at http://localhost:${port}`);
+console.log(`Serving statics from ${path.join(__dirname, '../')}`);
