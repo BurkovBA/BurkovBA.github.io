dc98cf7d6d74d3f2099f4c261f0cab751ae17499
Boris A. Burkov <vasjaforutube@gmail.com>
2019-07-08 10:42:06 +0300

    Fixed tags/categories.

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
   const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`);
-  const tagTemplate = path.resolve("src/templates/tags.js");
+  const tagTemplate = path.resolve("src/templates/tag.js");
 
   return graphql(
     `
