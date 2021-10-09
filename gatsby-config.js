354ae9b6381293607b5b16fbb28eb39a9a3d1cec
Boris A. Burkov <vasjaforutube@gmail.com>
2021-10-09 13:01:26 +0300

    Set up correct Google Analytics trackingId.

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,19 +71,19 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          `271884361`, // Google Analytics / GA
+          `UA-111349824-1`, // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
+          send_page_view: true
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
