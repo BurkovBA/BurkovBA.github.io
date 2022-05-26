56f9ee94e6e94441dba282570f33c8d1a25774e9
Boris A. Burkov <vasjaforutube@gmail.com>
2022-05-26 12:17:23 +0300

    Add duality gap animation.

--- content/blog/2022-02-25-1/index.md
+++ content/blog/2022-02-25-1/index.md
@@ -1,14 +1,16 @@
 ---
-title: Lagrange multipliers intuition and derivation
+title: Lagrange multipliers and duality
 date: "2022-05-10T00:00:00.284Z"
 tags: ["math"]
 cover: "./LagrangeMultipliers2D.png"
 description: Lagrange multipliers are ubiquitous in optimization theory and natural sciences, such as mechanics and statistical physics. Here I work out its intuition and derivation.
 ---
 
+## Constrained optimization
+
 ### Lagrange multipliers in 2D case
 
-Suppose that you need to find a maximum/minimum of a function $f(x, y)$, constrained on condition $g(x, y) = c$.
+Suppose that you need to find a maximum/minimum of a function $f(x, y)$, constrained on a condition $g(x, y) = c$.
 
 Direct solution of such a system would be as follows: using $g(x, y) = c$ condition we would express $y$ through $x$
 as $y = h(x)$, substitute this $y$ into $f(x, y) = f(x, h(x))$ and find the optimum of $f(x, h(x))$ in just variable, $x$.
@@ -30,6 +32,8 @@ See illustration of 2D case:
 
 ![Lagrange multipliers 2D scene](LagrangeMultipliers2DScene.mp4)
 
+### Lagrange multipliers in 3D case
+
 In 3D case similarly we optimize a function $f(x, y, z)$ constrained on two conditions $g_1(x, y, z) = c_1$ and $g_2(x, y, z) = c_2$.
 
 In this case intersection of curves $g_1(x, y, z) = c_1$ and $g_2(x, y, z) = c_2$ is approximated by a line. 
@@ -37,6 +41,27 @@ In this case intersection of curves $g_1(x, y, z) = c_1$ and $g_2(x, y, z) = c_2
 Again, $\nabla f(x, y, z)$ should be orthogonal to this intersection line. See illustration in 3D case:
 
 ![Lagrange multipliers 3D scene](LagrangeMultipliers3DScene.mp4)
+
+## Duality and Lagrange dual problem
+
+Instead of solving the original (or **primal**) constrained optimization problem, it is sometimes more convenient to
+solve a so-called **dual** problem.
+
+If the original problem is:
+
+$\begin{cases} f(x) \to \max \\ g(x) \le c \end{cases}$
+
+The Lagrange dual problem is:
+
+$\begin{cases} \lambda = \arg \min_x \Lambda(x, \lambda) = f(x) + \lambda g(x)  \\ g(x) \le c \end{cases}$
+
+If the functions $f(x)$ and $g(x)$ in the original problem were convex, the solutions of primal and dual problems are 
+equivalent (this is called Slater condition).
+
+However, if $f(x)$ and $g(x)$ are not convex, the solutions of the original problem $f^*(x)$ and of the Lagrange dual problem
+$\inf_{x} \Lambda(x)$ diverge by a value, called **duality gap**.
+
+![Lagrange dual](LagrangeDualScene.mp4)
 
 ## References:
 * https://math.stackexchange.com/questions/2578903/lagrange-multipliers-tangency - good stack overflow post on Lagrange multipliers derivation
---
+++ content/blog/2022-02-25-1/lagrange_dual.py
@@ -0,0 +1,93 @@
+from manimlib import *
+import numpy as np
+
+
+class LagrangeDualScene(Scene):
+    def construct(self):
+        # construct coordinate axes
+        axes_width_and_height = min(FRAME_HEIGHT, FRAME_WIDTH) - 1
+        axes = Axes(
+            (-10, 10), (-3, 10),
+            x_axis_config={"include_tip": True}, y_axis_config={"include_tip": True},
+            height=axes_width_and_height, width=axes_width_and_height
+        )
+        axes.add_coordinate_labels(
+            font_size=10,
+            num_decimal_places=1,
+        )
+        self.add(axes)
+        labels = axes.get_axis_labels(x_label_tex='g(x)', y_label_tex='f(x)').set_color(GREY)
+        self.add(labels)
+
+        # bezier curve
+        vertices = [
+            axes.c2p(0, 8, 0),
+            axes.c2p(2, 5, 0),
+            axes.c2p(4, 4, 0),
+            axes.c2p(2, 2, 0),
+            axes.c2p(0, 4, 0),
+            axes.c2p(-2, 5, 0),
+            axes.c2p(-4, 4, 0),
+            axes.c2p(-6, 6, 0),
+        ]
+        polygon = Polygon(*vertices, stroke_width=0, fill_opacity=1, fill_color=BLUE)
+        self.add(polygon)
+        self.wait()
+
+        # dual line
+        line_start = axes.c2p(-10, 6)
+        line_end = axes.c2p(11, -1)
+        line = DashedLine(start=line_start, end=line_end, color=RED)
+        self.add(line)
+        self.wait()
+
+        # line label
+        # line_label = Tex(f"f(x) = \Lambda - \lambda \cdot g(x)")
+        # line_label.set_bstroke(color=RED, width=50)
+        # line_label.arrange(RIGHT)
+        # always(line_label.next_to, line, UP)
+        # axes.add(line_label)
+
+        # dot
+        invisible_line = Line(start=axes.c2p(0, 0), end=axes.c2p(0, 2.66))
+        dual_dot = Dot(color=RED, point=axes.c2p(0, 0))
+        self.play(FadeIn(dual_dot, scale=0.5), MoveAlongPath(dual_dot, invisible_line))
+        lambda_text = Tex("\inf_x \Lambda(x) = f(x) - \lambda g(x)")
+        lambda_text.next_to(dual_dot, direction=LEFT)
+        lambda_text.set_color(RED)
+        self.add(lambda_text)
+        self.wait(3)
+
+        # f*(x) dot
+        invisible_line = Line(start=axes.c2p(0, 0), end=axes.c2p(0, 2))
+        dual_dot = Dot(color=GREEN, point=axes.c2p(0, 0))
+        self.play(FadeIn(dual_dot, scale=0.5), MoveAlongPath(dual_dot, invisible_line))
+
+        dual_line_start = axes.c2p(0, 2)
+        dual_line_end = axes.c2p(2, 2)
+        dual_line = DashedLine(start=dual_line_start, end=dual_line_end, color=GREEN)
+        self.add(dual_line)
+        self.wait()
+
+        f_star_text = Tex("f^*(x)")
+        f_star_text.next_to(dual_dot, direction=LEFT)
+        f_star_text.set_color(GREEN)
+        self.add(f_star_text)
+
+        self.wait(3)
+
+        # duality gap
+        self.play(FadeOut(f_star_text), FadeOut(lambda_text))
+
+        duality_gap_line_start = axes.c2p(0, 2)
+        duality_gap_line_end = axes.c2p(0, 2.5)
+        duality_gap_line = Line(start=duality_gap_line_start, end=duality_gap_line_end, color=BLUE)
+        duality_gap_line.set_stroke(width=10)
+        self.add(duality_gap_line)
+        self.wait()
+
+        duality_gap_text = Tex("gap = \inf_x \Lambda(x) - f^*(x)")
+        duality_gap_text.next_to(duality_gap_line, direction=LEFT)
+        duality_gap_text.set_color(BLUE)
+        self.add(duality_gap_text)
+        self.wait(3)
